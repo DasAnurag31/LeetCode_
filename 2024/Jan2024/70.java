5919120eb27f802d1175f8920afe9d56b94e33a3
Anurag Das <92816250+DasAnurag31@users.noreply.github.com>
2024-01-18 09:57:49 +0530

    Daily problem 18 Jan

---
+++ 2024/Jan2024/70.java
@@ -0,0 +1,22 @@
+class Solution {
+    private int count = 0;
+    private int helper(int n,int[] dp){
+        if(n == 0){
+            return 1;
+        }
+        if(n < 0){
+            return 0;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        int left = helper(n-1,dp);
+        int right = helper(n-2,dp);
+        return dp[n] = left + right;
+    }
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return helper(n,dp);
+    }
+}
