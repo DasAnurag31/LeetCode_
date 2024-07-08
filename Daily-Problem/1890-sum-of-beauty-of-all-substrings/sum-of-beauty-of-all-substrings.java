5c0b66497d200605a8217d906d40a3d31ad2893a
Anurag Das <dasanurag3129@gmail.com>
2024-07-09 00:30:00 +0530

    Time: 15 ms (100.00%) | Memory: 44.7 MB (24.36%) - LeetSync

--- Daily-Problem/1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
+++ Daily-Problem/1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -1,30 +1,39 @@
 class Solution {
     public int beautySum(String s) {
-        int sum = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            int[] charFreq = new int[26];            
-            for (int j = i; j < s.length(); j++) {
-                char c = s.charAt(j);
-                charFreq[c - 'a']++;
-                sum += beautySum(charFreq);
+        int size = s.length();
+        int total = 0;
+        char[] ss = s.toCharArray();
+        for (int left = 0; left < size; ++left) {
+            int[] freqs = new int[26];
+            freqs[ss[left] - 'a'] = 1;
+
+            int[] freqFreq = new int[size + 1];
+            freqFreq[1] = 1;
+
+            int max = 1;
+            int min = 1;
+
+            for (int right = left + 1; right < size; ++right) {
+                int i = ss[right] - 'a';
+
+                ++freqs[i];
+
+                max = Math.max(max, freqs[i]);
+
+                --freqFreq[freqs[i] - 1];
+                ++freqFreq[freqs[i]];
+
+                if (freqs[i] == 1) {
+                    min = 1;
+                }
+                else if (freqFreq[min] == 0) {
+                    min = freqs[i];
+                }
+
+                total += max - min;
             }
         }
-        
-        return sum;
-    }
-    
-    private int beautySum(int[] charFreq) {
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        
-        for (int i : charFreq) {
-            if (i != 0) {
-                max = Math.max(max, i);
-                min = Math.min(min, i);
-            }
-        }
-        
-        return max - min;
+
+        return total;
     }
 }
