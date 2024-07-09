5c3a8f64574a29ca36d786b7191d9bf46c06cc58
Anurag Das <dasanurag3129@gmail.com>
2024-07-09 18:40:38 +0530

    Time: 7 ms (51.28%), Space: 44.4 MB (33.74%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,32 @@
+class Solution {
+    private boolean dfs(
+        String s,
+        int prev,
+        int cur,
+        Set<String> wordDict,
+        Set<Integer> visited
+    ) {
+        if (cur == s.length()) {
+            return true;
+        }
+        if (visited.contains(prev)) {
+            return false;
+        }
+        for (int i = cur; i <= s.length(); i++) {
+            String partition = s.substring(prev, i);
+            if (wordDict.contains(partition)) {
+                if (dfs(s, i, i, wordDict, visited)) {
+                    return true;
+                }
+            }
+        }
+        visited.add(prev);
+        return false;
+    }
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>(wordDict);
+        Set<Integer> visited = new HashSet<>();
+        return dfs(s, 0, 0, wordSet, visited);
+    }
+} 
