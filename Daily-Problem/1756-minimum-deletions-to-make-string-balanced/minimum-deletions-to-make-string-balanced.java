e1ad1d3445e05a7f583d58dfc6b76283168af914
Anurag Das <dasanurag3129@gmail.com>
2024-07-30 14:58:11 +0530

    Time: 36 ms (44.57%) | Memory: 45.6 MB (23.12%) - LeetSync

--- Daily-Problem/1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ Daily-Problem/1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -6,22 +6,25 @@ class Solution {
         int[] postA=new int[s.length()];
         char[] ss=s.toCharArray();
         for(int i=0;i<ss.length;i++){
-            preB[i]=countB;
-            if(ss[i]=='b'){
+             if(ss[i]=='b'){
                 countB++;
             }
+            preB[i]=countB;
+           
         }
         for(int i=postA.length-1;i>=0;i--){
-            postA[i]=countA;
             if(ss[i]=='a'){
                 countA++;
             }
+            postA[i]=countA;
+            
         }
         int ans=Integer.MAX_VALUE;
         for(int i=0;i<ss.length;i++){
-            int totalCost=preB[i]+postA[i];
+            int totalCost=preB[i]+postA[i]-1;
             ans=Math.min(totalCost,ans);
         }
         return ans;
     }
 }
+
