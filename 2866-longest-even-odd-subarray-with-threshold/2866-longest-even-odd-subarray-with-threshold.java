30b80e62bd6e7204c6be046c860c1a304e25e017
Anurag Das <dasanurag3129@gmail.com>
2025-07-07 00:07:06 +0530

    Time: 349 ms (5.1%), Space: 45.3 MB (18.98%) - LeetHub

--- 2866-longest-even-odd-subarray-with-threshold/2866-longest-even-odd-subarray-with-threshold.java
+++ 2866-longest-even-odd-subarray-with-threshold/2866-longest-even-odd-subarray-with-threshold.java
@@ -3,9 +3,6 @@ class Solution {
         int max = 0;
         int left = 0, right = 0;
         while(right < nums.length){
-            
-
-
             while(left < nums.length && nums[left] % 2 != 0){
                 left++;
             }
@@ -15,24 +12,18 @@ class Solution {
             if(right == nums.length){
                 break;
             }
-            if((right - left) % 2 == 0){
-                if(nums[right] % 2 != 0){
-                    // shrink
-                    left++;
-                    continue;
-                }
+
+            if((right - left) % 2 == 0 && nums[right] % 2 != 0){
+                left++;
+                continue;
             }
-            if((right - left) % 2 != 0){
-                if(nums[right] % 2 == 0){
-                    // shrink
-                    left++;
-                    continue;
-                } 
+            if((right - left) % 2 != 0 && nums[right] % 2 == 0){
+                left++;
+                continue;
             }
             if(nums[right] > k){
                 left++;
-            }
-            else{
+            }else{
                 System.out.println(left + " " + right);
                 max = Math.max(max, right - left + 1);
                 right++;
